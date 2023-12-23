e4665ce29aeda24493b3d3d94fb196ed921c3cd4
luoshen19 <lkzc19@gmail.com>
2023-12-23 23:32:25 +0800

    feat: gateway

--- n-core/src/main/kotlin/xyz/drinkice/nahidalibrary/CoreApplication.kt
+++ n-core/src/main/kotlin/xyz/drinkice/nahidalibrary/CoreApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-open class StorageApplication
+open class CoreApplication
 
 fun main(args: Array<String>) {
-  runApplication<StorageApplication>(*args)
+  runApplication<CoreApplication>(*args)
 }
